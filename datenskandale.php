528313178bec703747300a82926ca91d6c88ff6e
pasbelg <pascal.belger7@gmail.com>
2019-06-03 12:32:50 +0200

    Readme and comments

--- datenskandale.php
+++ datenskandale.php
@@ -1,4 +1,8 @@
 <?php
+  /*Generieren von Skandalverläufe aller Datenskandale
+  Es wird fast der gleich Code wie bei "displaySkandale.php" verwendet es werden nur mehrere Skandalverläufe angezeigt.
+  Diese werden jedoch nach Datenskandalen gefiltert.
+  */
   error_reporting(0);
   require_once('functions/dirFunctions.php');
   require_once('functions/dataFunctions.php');
@@ -18,16 +22,22 @@
       </h1>
     </div>
     <?php
-      //print_r($skandalData);
+      
       foreach($skandalData as $key => $dates) {
         $skandalname = str_replace('json/', '', dirname($key, 1));
+        //Filterung von Skandalen welche "Daten" im Namen haben.
         if (strpos($skandalname, 'Daten')) {
+            //Sortieren der Datumsangaben von Früh zu Spät.
             usort($dates, "date_sort");
+            //Zählen der Veröffentlichungen pro Skandal.
             $occurences = array_count_values($dates);
+            //Speichern der Gesammtsumme um den Prozentualen Anteil zu berechnen
             $occTotal = array_sum($occurences);
+            //Ausgabe der Abschnitte welchen die einzelnen Datenskandale enthalten
             echo ' <div class = "box" id = "horizontalChartBox"><h2> Artikelverlauf von ' . $skandalname . ' </h2> <div id = "horizontalChart"> ';
             foreach($occurences as $date => $occurence) {
                 $date = date("d.m. ", strtotime($date));
+                //Verschiedene Säulenhöhe von Skandalen mit großem oder kleinem Medieninteresse
                 if ($occTotal> 50 && array_count_values($dates) > 3) {
                     $barHeight = 30 + ($occurence * 100 / $occTotal) * 35;
                     if ($occurence>= 3) {
