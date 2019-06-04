0d5d6f903dd5e77730fda48c461d0d49a1332842
pasbelg <pascal.belger7@gmail.com>
2019-06-04 12:54:50 +0200

    commenting

--- python/removeComment.py
+++ python/removeComment.py
@@ -3,7 +3,7 @@
 - Suchanfrage auf Nexis
 - Dokumente als HTML (Alle Haken herausnehmen) in 200er Batches herunterladen (Limit von Nexis) und Speichern
 
-# Imports und Setup
+# Imports
 """
 import os, re
 from bs4 import BeautifulSoup
@@ -15,16 +15,21 @@ htmlFiles = os.listdir(HTMLpath)
 for file in htmlFiles:
     # Ausschließen von Dateien mit anderen Dateiendungen als HTML.
     if file.find('.HTML') != -1:
-        fileName=(os.path.splitext(file)[0])
+        # Speichern des Dateinamens ohne Format.
+        fileName = (os.path.splitext(file)[0])
         print(fileName)
+        # Definieren der Verzeichnisse aus den Dateinamen der HTML-Datei.
         writePath='./html/changed/' + fileName + '/'
+        # Entfernen aller Kommentare in den HTML-Dateien.
         with open(HTMLpath + file, 'rb') as html:
             string = html.read()
             removeCommentTag = re.sub('<!--|-->', '', str(string))
+            # Erstellen der Verzeichnisse pro HTML-Datei.
             try:  
                 os.mkdir(writePath)
             except OSError:  
                 print ("Creation of the directory failed")
             print (writePath)         
+            # Speichern der kommentarfreien HTML-Dateien in den angelegten Verzeichnissen.
             with open(writePath + file, 'w', encoding='utf8') as f:
                     f.write(removeCommentTag)
